d185f66519afd22e29129edc43e36fc7f3c044f2
Bowie <lfranckx@thinkful.com>
2021-03-08 14:14:12 -0800

    log res messages

--- src/services/RxNormApiService.js
+++ src/services/RxNormApiService.js
@@ -1,23 +1,23 @@
 const RxApiService = {
     searchByTerm(term) {
         console.log('term', term);
-        // return fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?drugs?name=${term}`, {
-        //     method: 'GET',
-        //     headers: {
-        //         'content-type': 'application-json'
-        //     }
-        // })
-        return fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=11523-7020-1`, {
+        return fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?drugs?name=${term}`, {
             method: 'GET',
             headers: {
                 'content-type': 'application/json'
             }
         })
+        // return fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=11523-7020-1`, {
+        //     method: 'GET',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     }
+        // })
         .then((res) =>
-            console.log(res)
-            // !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+        console.log(res)
+        // !res.ok ? res.json().then((e) => Promise.reject(e)) : console.log(res.json())
+        // !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
         ) 
-        // .then((response) => response.json())
     }
 }
 
